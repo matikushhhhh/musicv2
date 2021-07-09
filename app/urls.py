ec7d9ed53c154910639711194cdebcede9a89bad
Manuel esteban <manux518@gmail.com>
2021-07-09 01:56:48 -0400

    fsaf

--- app/urls.py
+++ app/urls.py
@@ -5,23 +5,16 @@ from django.urls import path, include
 from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework import routers
-from .views import tbk, statusTrx
 
-router = routers.DefaultRouter()
-router.register('productos', ProductoViewSet)
+
 
 urlpatterns = [
     path('', home, name="home"),
-    path('tienda/',tienda, name="tienda"),
-	path('carrito/', carrito, name="carrito"),
-	path('main/', main, name="main"),
-    path('pagos/', pagos, name="pagos"),
-    path('api/' , include(router.urls)),
-    path('tbk/', tbk, name="tbk"),
-    path('tbkRes', tbk, name="tbkRes"),
-    path('statusTrx/', statusTrx, name="statusTrx")
-    ]
+	path('', store, name="store"),
+	path('cart/', cart, name="cart"),
+	path('checkout/', checkout, name="checkout"),
+
+]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
--- app/views.py
+++ app/views.py
@@ -20,68 +20,16 @@ def home(request):
     return render(request, 'app/home.html')
 
 
+from django.shortcuts import render
 
-def tienda(request):
-    productos = Producto.objects.all()
-    context ={'productos':productos}
-    return render (request, 'app/tienda.html', context)
+def store(request):
+	context = {}
+	return render(request, 'store/store.html', context)
 
+def cart(request):
+	context = {}
+	return render(request, 'store/cart.html', context)
 
-def carrito(request):
-    if request.user.is_authenticated:
-        cliente = request.user.cliente
-        pedido, created = Pedido.objects.get_or_create(cliente = cliente, complete = False)
-        items = pedido.pedidoproducto_set.all()
-    else:
-        items = [] 
-        pedido = {'get_cart_total' : 0, 'get_cart_items': 0}  
-
-    context ={'items': items, 'pedido':pedido}
-    return render (request, 'app/carrito.html', context)
-    
-
-def main(request):
-    context ={}
-    return render (request, 'app/main.html', context)
-
-
-
-def pagos(request):
-    if request.user.is_authenticated:
-        cliente = request.user.cliente
-        pedido, created = Pedido.objects.get_or_create(cliente = cliente, complete = False)
-        items = pedido.pedidoproducto_set.all()
-    else:
-        items = [] 
-        pedido = {'get_cart_total' : 0, 'get_cart_items': 0}  
-
-    context ={'items': items, 'pedido':pedido}
-    return render (request, 'app/pagos.html', context)
-    
-def login(request):
-    return render(request, 'registration/login.html')
-
-
-class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Producto.objects.all()
-    serializer_class = ProductoSerializer
-    def get_queryset(self):
-        productos = Producto.objects.all()
-        nombre = self.request.GET.get('nombre')
-
-        if nombre:
-            productos = productos.filter(nombre=nombre)
-        return productos
-
-def tbk(request):
-    data = {
-        'resultado': get_initTrxTBK()
-    }
-    return render(request, 'app/initTrxTbk.html',data)
-
-@csrf_exempt
-def statusTrx(request):
-    data = {
-        'resultado': get_statusTBK(request)
-    }
-    return render(request, 'app/statusTrx.html',data)
+def checkout(request):
+	context = {}
+	return render(request, 'store/checkout.html', context)
