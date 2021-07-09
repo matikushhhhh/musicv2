ded3f570c6e16c2e4cf3642d1d77d96093d26a16
Manuel esteban <manux518@gmail.com>
2021-07-09 17:28:57 -0400

    carrit listo

--- app/static/js/cart.js
+++ app/static/js/cart.js
@@ -25,13 +25,14 @@ function updateUserOrder(productId, action){
 			method:'POST',
 			headers:{
 				'Content-Type':'application/json',
+                'X-CSRFToken' : csrftoken,
 			}, 
-			body:JSON.stringify({'productId':productId, 'action':action})
+			body:JSON.stringify({'productId': productId, 'action':action})
 		})
 		.then((response) => {
 		   return response.json();
 		})
 		.then((data) => {
-		    console.log('Data:', data)
+		    location.reload()
 		});
 }
