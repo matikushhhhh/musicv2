ce3dea991215a98a81a62a554667cb3f8969df25
Manuel esteban <manux518@gmail.com>
2021-07-09 03:27:36 -0400

    carrito links

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', home, name="home"),
-	path('', store, name="store"),
+	path('store/', store, name="store"),
 	path('cart/', cart, name="cart"),
 	path('checkout/', checkout, name="checkout"),
 
--- app/views.py
+++ app/views.py
@@ -23,7 +23,7 @@ def home(request):
 def store(request):
 	products = Product.objects.all()
 	context = {'products':products}
-	return render(request, 'store/store.html', context)
+	return render(request, 'app/store.html', context)
 
 def cart(request):
 
@@ -37,8 +37,8 @@ def cart(request):
 		order = {'get_cart_total':0, 'get_cart_items':0}
 
 	context = {'items':items, 'order':order}
-	return render(request, 'store/cart.html', context)
+	return render(request, 'app/cart.html', context)
 
 def checkout(request):
 	context = {}
-	return render(request, 'store/checkout.html', context)
+	return render(request, 'app/checkout.html', context)
